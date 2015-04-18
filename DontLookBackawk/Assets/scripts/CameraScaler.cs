69b3c0e32420521467c6b352af97eacebc46b9ae
Lachlan Ford <fordyfor@hotmail.com>
2015-04-18 15:44:32 +1000

    api update

--- DontLookBackawk/Assets/scripts/CameraScaler.cs
+++ DontLookBackawk/Assets/scripts/CameraScaler.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class CameraScaler : MonoBehaviour {
 	void Start () {
-		float scale = Mathf.Ceil(Screen.height/8);
-		Camera.main.orthographicSize = Screen.height / 2f / scale;
+		Screen.SetResolution(800, 600, false);
+		/*
+		Screen.SetResolution(800, 600, true);
+		float UnitsPerPixel = 1f / 50f;
+
+		Debug.Log(Screen.height);
+		Debug.Log("camera scale " + Screen.height / 2f * UnitsPerPixel);
+
+		Camera.main.orthographicSize =
+			Screen.height / 2f // ortho-size is half the screen height...
+				* UnitsPerPixel;
+		*/
 	}
 }
