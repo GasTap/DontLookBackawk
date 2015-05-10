4c94dce46b545026b3b184b2dc3e09a30b35c0be
Lachlan Ford <fordyfor@hotmail.com>
2015-05-10 14:31:01 +1000

    added menu and egg breaking

---
+++ DontLookBackawk/Assets/scripts/RemoveWhenOffScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveWhenOffScreen : MonoBehaviour {
+
+	Transform t;
+	float boundX = 7;
+	float boundY;
+	void Start () {
+		t = this.gameObject.transform;
+		boundY = boundX * (3.0f / 4.0f);
+	}
+
+	void Update () {
+		var p = t.position;
+		// TODO factor in width and height
+		if (p.x < -boundX) {
+			Destroy(this.gameObject);
+		} else if (p.x > boundX) {
+			Destroy(this.gameObject);
+		} else if (p.y < -boundY) {
+			Destroy(this.gameObject);
+		} else if (p.y > boundY) {
+			Destroy(this.gameObject);
+		}
+	}
+}
