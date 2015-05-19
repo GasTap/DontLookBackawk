563abc04b9cf1df1e591106d4d9a39a0f9ba0693
iuyhcdfs <iuyhcdfs@gmail.com>
2015-05-19 12:54:01 +1000

    eagle flies... too much. next: flying right + ai does what i envisioned

---
+++ DontLookBackawk/Assets/scripts/actor/EagleAI.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EagleAI : AIBehaviour {
+
+	private int timer; // count up from zero
+
+	private Vector2 lineOfSight;
+
+	private Vector2 center; // "center of mass" for patrol area
+	private float dy = 0.8f; // allowed deviance y velocity
+	private float deadZoneX = 1f; // small area where bird doesnt accel 
+
+	private enum State {
+		PATROL,
+		ATTACKING,
+		RETREATING,
+		PANIC
+		//WANDER // no aggro + explore new areas
+	}
+
+	private State currentState = State.PATROL;
+	private EagleActor actor;
+
+	void Start () {
+		actor = this.gameObject.GetComponent<EagleActor>();
+		center.x = 0f;
+		center.y = 3f;
+		lineOfSight = actor.atkVec;
+		timer = 0;
+	}
+
+	void Update () {
+
+		timer++;
+
+		// ! patrol until target spotted (visionBox Component)
+		if (currentState == State.PATROL) {
+			
+			if(this.transform.position.y < center.y - dy){
+				if(timer > 5){
+					actor.control_jump();
+					if(timer < 8){
+						timer = 0;
+					}
+				}
+			}
+			if(this.transform.position.x < center.x - deadZoneX){
+				actor.control_right();
+			}
+			if(this.transform.position.x > center.x + deadZoneX){
+				actor.control_left();
+			}
+			/*
+			lineOfSight = actor.atkVec;
+			RaycastHit2D target = Physics2D.Raycast(transform.position, lineOfSight);
+
+			if(target.collider.gameObject.ToString() == "Player"){
+				Debug.Log("found player!");
+				timer = 0;
+				currentState = State.ATTACKING;
+			}
+			if(target.collider.gameObject.ToString() == "Egg"){
+				Debug.Log("found egg!");
+				timer = 0;
+				currentState = State.ATTACKING;
+			}
+			*/
+		}
+
+		// ! do dive at some fixed velocity
+		// ! need to collide with ground and sweep along ground for a bit
+		if (currentState == State.ATTACKING) {
+			actor.control_attack();
+			if(timer > 25){
+				timer = 0;
+				currentState = State.RETREATING;
+			}
+		}
+
+		// thankfully, we are using the same collider for OWPlatforms.
+		// what if its not one way?...
+		if (currentState == State.RETREATING) {
+			if(actor.transform.position.y < center.y){
+				actor.control_jump ();
+			}
+			if(timer > 50){
+				timer = 0;
+				currentState = State.PANIC;
+			}
+		}
+
+		// something wrong happens itd be funny if the bird panics
+		if (currentState == State.PANIC) {}
+		// TODO decide if it goes below the stage, does it return?
+	}
+
+}
--- DontLookBackawk/Assets/scripts/actor/EagleActor.cs
+++ DontLookBackawk/Assets/scripts/actor/EagleActor.cs
@@ -3,39 +3,80 @@ using System.Collections;
 
 public class EagleActor : MonoBehaviour, ActorBehaviour {
 
-	public float accelX;
-	public float flapY;
-	public Vector2 atkVec;
+	// flying
+	public float accelX = 0.8f;
+	public float accelY = 0.005f;
+	public float maxVelX = 5f;
+	public float maxVelY = 0.8f;
+
+	// attacking
+	public Vector2 atkVec = new Vector2 (-2, -10);
+	public Vector2 maxAtkV = new Vector2 (10, 3);
 	public bool hasVision;
 	public bool hasVictim;
 
-	// Use this for pizza cake
+	private Rigidbody2D rb;
+
+	private enum State {
+		FLYING,
+		ATTACKING
+	}
+	private State currentState = State.FLYING;
+
 	void Start () {
-		// nuffin.
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+	void Update (){
+		capVelocity (rb.velocity);
 	}
 
 	public void UpdateActor (){
-		// move the transform.. i guess handle states if need be
 	}
 	public void control_left (){
-		// apply -accelX
+		if(rb.velocity.x > -maxVelX){
+			rb.velocity = new Vector2 (rb.velocity.x - accelX, rb.velocity.y);
+		}
+		if (atkVec.x > 0 && currentState == State.FLYING) {
+			atkVec.x = -atkVec.x;
+		}
 	}
 	public void control_right (){
-		// apply accelX
+		if (rb.velocity.x < maxVelX) {
+			rb.velocity = new Vector2 (rb.velocity.x + accelX, rb.velocity.y);
+		}
+		if (atkVec.x < 0 && currentState == State.FLYING) {
+			atkVec.x = -atkVec.x;
+		}
 	}
 	public void control_attack (){
 		// KILL horizontal, vertical accel
+		rb.velocity = new Vector2 (atkVec.x, atkVec.y);
 		// apply attackvector rapidly
-		// engage hitboxes
+		this.currentState = State.ATTACKING;
+
+		// TODO: engage hitboxes
 	}
 	public void control_jump (){
-		// apply flapy
+		if (rb.velocity.y < maxVelY) {
+			rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + accelY);
+		}
 	}
 	public void control_still (){
 		// kill velocity
+		//rb.velocity = new Vector2 (0, 0);
 	}
 	
 	public void control_special (){}
 	public void control_die (){}
 	public void control_take_damage (float amount){}
+
+	private Vector2 capVelocity (Vector2 capMe){
+		if(rb.velocity.x < 0){
+			return new Vector2(Mathf.Max(capMe.x, -maxVelX), Mathf.Min(capMe.y, maxVelY));
+		}
+		if(rb.velocity.x > 0){
+			return new Vector2(Mathf.Min(capMe.x, maxVelX), Mathf.Min(capMe.y, maxVelY));
+		}
+		return new Vector2 (capMe.x, Mathf.Min(capMe.y, maxVelY));
+	}
 }
